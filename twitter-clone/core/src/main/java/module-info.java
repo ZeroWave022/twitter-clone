7749597a5cba68d1cf4e002160e07c0f55a564fd
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-09 13:34:48 +0200

    refactor: update module names and delete template files

--- twitter-clone/core/src/main/java/module-info.java
+++ twitter-clone/core/src/main/java/module-info.java
@@ -1,3 +1,3 @@
-module calc.core {
+module twitter.core {
   exports core;
 }
--- twitter-clone/ui/src/main/java/module-info.java
+++ twitter-clone/ui/src/main/java/module-info.java
@@ -1,5 +1,5 @@
-module calc.ui {
-  requires calc.core;
+module twitter.ui {
+  requires twitter.core;
   requires javafx.controls;
   requires javafx.fxml;
 
