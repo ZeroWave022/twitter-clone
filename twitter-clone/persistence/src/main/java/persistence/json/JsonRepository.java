53e3dbb6b7bbda287fed1c6722e65aefce4545f5
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-09 17:59:25 +0200

    feat: add user repository for user persistence

---
+++ twitter-clone/persistence/src/main/java/persistence/json/JsonRepository.java
@@ -0,0 +1,34 @@
+package persistence.json;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonRepository<T> {
+  private final ObjectMapper objectMapper;
+  private final Path filePath;
+  private final TypeReference<List<T>> typeReference;
+
+  public JsonRepository(Path filePath, TypeReference<List<T>> typeReference) {
+    this.objectMapper = new ObjectMapper();
+    this.filePath = filePath;
+    this.typeReference = typeReference;
+  }
+
+  public List<T> load() throws IOException {
+    File file = this.filePath.toFile();
+    if (!file.exists() || file.length() == 0) {
+      return new ArrayList<>();
+    }
+
+    return this.objectMapper.readValue(file, this.typeReference);
+  }
+
+  public void save(List<T> data) throws IOException {
+    this.objectMapper.writeValue(this.filePath.toFile(), data);
+  }
+}
