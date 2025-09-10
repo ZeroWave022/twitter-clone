cbe941e0815189f98b19fcbcd57fb18b4d7710f2
Adrian Ramberg Westgaard <adrianrw@stud.ntnu.no>
2025-09-10 22:04:55 +0200

    chore: added App.java for launching log in screen

---
+++ twitter-clone/ui/src/main/java/ui/App.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage.setTitle("Twitter Clone");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
