224e3be38c0daf20dcde602f9a796d9810a667bf
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-14 22:18:21 +0200

    fix: suppress bug warnings

--- twitter-clone/core/src/main/java/core/Post.java
+++ twitter-clone/core/src/main/java/core/Post.java
@@ -4,101 +4,106 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class Post {
 
-    public static final int MAX_CONTENT_LENGTH = 280;
+  public static final int MAX_CONTENT_LENGTH = 280;
 
-    private String id;
-    private User originalPoster;
-    private String content;
-    // private Instant createdAt;
+  private String id;
+  private User originalPoster;
+  private String content;
+  // private Instant createdAt;
 
-    private int likes = 0;
-    private int reTweets = 0;
-    private int commentsAmount = 0;
+  private int likes = 0;
+  private int reTweets = 0;
+  private int commentsAmount = 0;
 
-    public Post(User user, String content, String id) {
-        setOriginalPoster(user);
-        setContent(content);
-        setId(id);
-        // this.createdAt = Instant.now();
+  @SuppressFBWarnings(value = "CT_CONSTRUCTOR_THROW")
+  public Post(User user, String content, String id) {
+    setOriginalPoster(user);
+    setContent(content);
+    setId(id);
+    // this.createdAt = Instant.now();
+  }
+
+  public Post() {
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public int getLikes() {
+    return likes;
+  }
+
+  public int getCommentsAmount() {
+    return commentsAmount;
+  }
+
+  // public Instant getCreatedAt() {
+  // return createdAt;
+  // }
+
+  public static int getMaxContentLength() {
+    return MAX_CONTENT_LENGTH;
+  }
+
+  @SuppressFBWarnings(value = "EI_EXPOSE_REP")
+  public User getOriginalPoster() {
+    return originalPoster;
+  }
+
+  public int getReTweets() {
+    return reTweets;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setContent(String content) {
+    if (content == null) {
+      throw new IllegalArgumentException("Content cannot be null");
     }
-
-    public Post(){
+    if (content.isEmpty()) {
+      throw new IllegalArgumentException("Content can not be emtpy");
     }
-
-    public String getContent() {
-        return content;
+    if (content.length() > MAX_CONTENT_LENGTH) {
+      throw new IllegalArgumentException("Content is to long");
     }
+    this.content = content;
+  }
 
-    public int getLikes() {
-        return likes;
+  public void setCommentsAmount(int commentsAmount) {
+    this.commentsAmount = commentsAmount;
+  }
+
+  // public void setCreatedAt(Instant createdAt) {
+  // this.createdAt = createdAt;
+  // }
+
+  public void setId(String id) {
+    if (id == null) {
+      throw new IllegalArgumentException("Id cannot be null");
     }
+    this.id = id;
+  }
 
-    public int getCommentsAmount() {
-        return commentsAmount;
+  public void setLikes(int likes) {
+    this.likes = likes;
+  }
+
+  @SuppressFBWarnings(value = "EI_EXPOSE_REP2")
+  public void setOriginalPoster(User originalPoster) {
+    if (originalPoster == null) {
+      throw new IllegalArgumentException("User cannot be null");
     }
+    this.originalPoster = originalPoster;
+  }
 
-    // public Instant getCreatedAt() {
-    //     return createdAt;
-    // }
-
-    public static int getMaxContentLength() {
-        return MAX_CONTENT_LENGTH;
-    }
-
-    public User getOriginalPoster() {
-        return originalPoster;
-    }
-
-    public int getReTweets() {
-        return reTweets;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setContent(String content) {
-        if (content == null) {
-            throw new IllegalArgumentException("Content cannot be null");
-        }
-        if (content.isEmpty()) {
-            throw new IllegalArgumentException("Content can not be emtpy");
-        }
-        if (content.length() > MAX_CONTENT_LENGTH) {
-            throw new IllegalArgumentException("Content is to long");
-        }
-        this.content = content;
-    }
-
-    public void setCommentsAmount(int commentsAmount) {
-        this.commentsAmount = commentsAmount;
-    }
-
-    // public void setCreatedAt(Instant createdAt) {
-    //     this.createdAt = createdAt;
-    // }
-
-    public void setId(String id) {
-        if (id == null){
-            throw new IllegalArgumentException("Id cannot be null");
-        }
-        this.id = id;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    public void setOriginalPoster(User originalPoster) {
-        if (originalPoster == null) {
-            throw new IllegalArgumentException("User cannot be null");
-        }
-        this.originalPoster = originalPoster;
-    }
-
-    public void setReTweets(int reTweets) {
-        this.reTweets = reTweets;
-    }
+  public void setReTweets(int reTweets) {
+    this.reTweets = reTweets;
+  }
 }
