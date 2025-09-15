97fae9cc393690540884abd232083ec6001e533d
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-15 20:13:49 +0200

    config: update module-info

--- twitter-clone/ui/src/main/java/module-info.java
+++ twitter-clone/ui/src/main/java/module-info.java
@@ -3,5 +3,11 @@ module twitter.ui {
   requires javafx.controls;
   requires javafx.fxml;
 
-  opens ui to javafx.graphics, javafx.fxml;
+  requires spring.boot;
+  requires spring.boot.autoconfigure;
+  requires spring.context;
+  requires spring.beans;
+  requires spring.core;
+
+  opens ui to javafx.graphics, javafx.fxml, spring.core, spring.beans, spring.context;
 }
