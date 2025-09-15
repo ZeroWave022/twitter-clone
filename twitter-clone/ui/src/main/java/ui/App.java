033e5485fa9f3037d476b8990e1dead41af3a872
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-15 20:23:49 +0200

    feat: initialize springboot in App

--- twitter-clone/ui/src/main/java/ui/App.java
+++ twitter-clone/ui/src/main/java/ui/App.java
@@ -1,34 +1,54 @@
 package ui;
 
 import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+@SpringBootApplication(scanBasePackages = { "twitter.ui", "twitter.persistence" })
 public class App extends Application {
-    private static Scene scene;
+  private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene newScene = new Scene(root, 1280, 720);
-        stage.setTitle("Twitter Clone");
-        stage.setScene(newScene);
-        stage.show();
-        setScene(newScene);
-    }
+  private ConfigurableApplicationContext springContext;
 
-    private static void setScene(Scene newScene) {
-        scene = newScene;
-    }
+  @Override
+  public void init() throws Exception {
+    springContext = SpringApplication.run(App.class);
+  }
 
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(FXMLLoader.load(App.class.getResource(fxml)));
-    }
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+    Scene newScene = new Scene(root, 1280, 720);
+    stage.setTitle("Twitter Clone");
+    stage.setScene(newScene);
+    stage.show();
+    setScene(newScene);
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  @Override
+  public void stop() throws Exception {
+    springContext.close();
+    Platform.exit();
+  }
+
+  private static void setScene(Scene newScene) {
+    scene = newScene;
+  }
+
+  public static void setRoot(String fxml) throws IOException {
+    scene.setRoot(FXMLLoader.load(App.class.getResource(fxml)));
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
