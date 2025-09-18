6e82f6c54a672200500f9af6ec5ef34f8629ab4c
Adrian <adrian.ramberg.westgaard@hotmail.com>
2025-09-18 17:39:42 +0200

    feat: integrated log in with user repo

--- twitter-clone/ui/src/main/java/ui/Controller.java
+++ twitter-clone/ui/src/main/java/ui/Controller.java
@@ -1,12 +1,15 @@
 package ui;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import core.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import service.PostService;
 import service.UserService;
@@ -23,38 +26,59 @@ public class Controller {
   }
 
   @FXML
-  private TextField username;
+  private TextField usernameField;
 
   @FXML
-  private TextField password;
+  private TextField passwordField;
 
   @FXML
+  @SuppressWarnings("unused")
   private Button logInBtn;
 
-  // TODO: Make auth function, possibly move to core
-  public boolean auth() { // (String username, String password)
-    return true;
+  @FXML
+  private Label errorLabel;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private void logIn() throws IOException { // (String username, String password)
+    // Save input to variables
+    String username = usernameField.getText();
+    String password = passwordField.getText();
+
+    Optional<User> maybeUser = userService.getUserByUsername(username);
+
+    // User exists, confirm password
+    if (maybeUser.isPresent()) {
+      User user = maybeUser.get();
+      if (user.getPassword().equals(password)) {
+        App.setRoot("feed.fxml");
+      } else {
+        errorLabel.setText("Incorrect password");
+      }
+    }
+    // New user gets to log in
+    else {
+      User newUser = new User(null, username, username, password); // make new user, repo handles ID
+      userService.createUser(newUser); // add user to db
+      App.setRoot("feed.fxml"); // switch to feed
+    }
   }
 
-  // Meant to be called from log in, auth(String username, String password) needs
-  // to be implemented
   @FXML
+  @SuppressWarnings("unused")
   private void switchToFeed() throws IOException {
-    if (!auth()) {
-      System.out.println("Credentials not approved");
-      return;
-    }
-
     App.setRoot("feed.fxml");
   }
 
   @FXML
+  @SuppressWarnings("unused")
   private void switchToMakeNewPost() throws IOException {
     App.setRoot("makeNewPost.fxml");
   }
 
   // TODO: Implement method and integrate with tweet code class
   @FXML
+  @SuppressWarnings("unused")
   private void publishPost() throws IOException {
     System.out.println("Post wanting to be published");
 
