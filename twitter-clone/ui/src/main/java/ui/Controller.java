6736b25ed0c4a6980e3613e0e9975c2f399aa582
Adrian Ramberg Westgaard <adrianrw@stud.ntnu.no>
2025-09-22 16:47:03 +0200

    fix: Moved logic and set loggedInUser
    The logic of logging in has been moved from controller.java to
    UserService.java, controller will now only handle ui changes.
    
    Additionally, loggedInUser is now set for userService making it easy
    to get info/posts and so on of the current user
    
    Co-authored-by: Daniel <danielgg@stud.ntnu.no>

--- twitter-clone/ui/src/main/java/ui/Controller.java
+++ twitter-clone/ui/src/main/java/ui/Controller.java
@@ -41,34 +41,17 @@ public class Controller {
   @FXML
   @SuppressWarnings("unused")
   private void logIn() throws IOException {
-    try {
+    // clear any errors
+    errorLabel.setText("");
 
-      // clear any errors
-      errorLabel.setText("");
+    // Save input to variables
+    String username = usernameField.getText();
+    String password = passwordField.getText();
 
-      // Save input to variables
-      String username = usernameField.getText();
-      String password = passwordField.getText();
-
-      Optional<User> maybeUser = userService.getUserByUsername(username);
-
-      // User exists, confirm password
-      if (maybeUser.isPresent()) {
-        User user = maybeUser.get();
-        if (user.getPassword().equals(password)) {
-          App.setRoot("feed.fxml");
-        } else {
-          errorLabel.setText("Incorrect Password");
-        }
-      }
-      // New user gets to log in
-      else {
-        User newUser = new User(null, username, username, password); // make new user, repo handles ID
-        userService.createUser(newUser); // add user to db
-        App.setRoot("feed.fxml"); // switch to feed
-      }
-    } catch (IOException e) {
-      errorLabel.setText("Something went wrong");
+    if (userService.logIn(username, password)) {
+      App.setRoot("feed.fxml");
+    } else {
+      errorLabel.setText("Incorrect password");
     }
   }
 
