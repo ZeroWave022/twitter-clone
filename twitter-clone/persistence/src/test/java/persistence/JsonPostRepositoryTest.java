a4f4be0aecc2f31d671b370191a5a143f40418aa
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-23 18:25:53 +0200

    test: update persistence tests

--- twitter-clone/persistence/src/test/java/persistence/JsonPostRepositoryTest.java
+++ twitter-clone/persistence/src/test/java/persistence/JsonPostRepositoryTest.java
@@ -12,18 +12,19 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import core.Post;
 import core.User;
 
 class JsonPostRepositoryTest {
+  @TempDir
   private Path dataDir;
   private JsonPostRepository postRepository;
 
   @BeforeEach
   void setup() throws IOException {
-    this.dataDir = Files.createTempDirectory("users");
-    this.postRepository = new JsonPostRepository(this.dataDir);
+    this.postRepository = new JsonPostRepository(dataDir.toAbsolutePath().toString());
   }
 
   @AfterEach
@@ -57,7 +58,7 @@ class JsonPostRepositoryTest {
     List<Post> posts = this.postRepository.findAll();
     assertEquals(1, posts.size());
 
-    JsonPostRepository loadedRepository = new JsonPostRepository(this.dataDir);
+    JsonPostRepository loadedRepository = new JsonPostRepository(this.dataDir.toAbsolutePath().toString());
     List<Post> loadedPosts = loadedRepository.findAll();
     assertEquals(1, loadedPosts.size());
   }
