b3eefe001f7acca0e31a891367491f3701805819
Martin Alexander Kaminski <martakam@stud.ntnu.no>
2025-09-28 15:28:24 +0200

    feat: Add new PostRepository using Hibernate

---
+++ twitter-clone/persistence/src/main/java/persistence/OrmPostRepository.java
@@ -0,0 +1,48 @@
+package persistence;
+
+import core.Post;
+import java.util.List;
+import java.util.Optional;
+import persistence.hibernate.HibernateRepository;
+
+public class OrmPostRepository extends HibernateRepository implements PostRepository {
+  @Override
+  public void deleteById(Long id) {
+    sessionFactory.inTransaction(session -> {
+      Post post = session.find(Post.class, id);
+      if (post != null) {
+        session.remove(post);
+      }
+    });
+  }
+
+  @Override
+  public boolean existsById(Long id) {
+    return sessionFactory.fromTransaction(session -> {
+      return session.find(Post.class, id) != null;
+    });
+  }
+
+  @Override
+  public List<Post> findAll() {
+    return sessionFactory.fromTransaction(session -> {
+      return session.createSelectionQuery("from Post", Post.class).getResultList();
+    });
+  }
+
+  @Override
+  public Optional<Post> findById(Long id) {
+    return sessionFactory.fromTransaction(session -> {
+      Post post = session.find(Post.class, id);
+      return Optional.ofNullable(post);
+    });
+  }
+
+  @Override
+  public Post save(Post post) {
+    sessionFactory.inTransaction(session -> {
+      session.persist(post);
+    });
+    return post;
+  }
+}
