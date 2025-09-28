1474d61b804d9d8cdad15dafda20fbdd59f8e764
Henrik Strøm-Andersen <hestro@stud.ntnu.no>
2025-09-28 17:54:25 +0200

    feat: Add PostCell.java class
    This class will be used as a custom ListCell for Post objects by setting it as the ListView's cell factory

---
+++ twitter-clone/ui/src/main/java/ui/PostCell.java
@@ -0,0 +1,31 @@
+package ui;
+
+import core.Post;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+
+// This class is based on the example from this StackOverflow example:
+// https://stackoverflow.com/a/77508842
+public class PostCell extends ListCell<Post> {
+
+  @Override
+  protected void updateItem(Post post, boolean empty) {
+    super.updateItem(post, empty);
+    if (empty || post == null) {
+      setGraphic(null);
+    } else {
+      try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("post.fxml"));
+        HBox postItem = loader.load();
+        PostController postController = loader.getController();
+
+        postController.setData(post);
+        setGraphic(postItem);
+
+      } catch (Exception e) {
+        setGraphic(null);
+      }
+    }
+  }
+}
