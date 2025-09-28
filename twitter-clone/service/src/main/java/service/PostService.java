6024d7f16f1bb1cb8e5cea49954d05b42f2471d0
Henrik Strøm-Andersen <hestro@stud.ntnu.no>
2025-09-28 22:25:49 +0200

    feat: Add getAllPosts() in PostService to retrieve all posts from repo

--- twitter-clone/service/src/main/java/service/PostService.java
+++ twitter-clone/service/src/main/java/service/PostService.java
@@ -2,6 +2,8 @@ package service;
 
 import core.Post;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,5 +33,10 @@ public class PostService {
     }
 
     return this.postRepository.save(post);
+  }
+
+  @Transactional(readOnly = true)
+  public List<Post> getAllPosts() {
+    return this.postRepository.findAll();
   }
 }
