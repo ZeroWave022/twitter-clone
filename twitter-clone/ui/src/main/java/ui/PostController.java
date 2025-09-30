c88b52948654c06e87975fde5639ae6a6cce32d4
Adrian <adrian.ramberg.westgaard@hotmail.com>
2025-10-01 01:37:17 +0200

    feat: added basic like functionality, incrementing the relevant posts likes by 1 each time the button is clicked

--- twitter-clone/ui/src/main/java/ui/PostController.java
+++ twitter-clone/ui/src/main/java/ui/PostController.java
@@ -1,12 +1,27 @@
 package ui;
 
-import core.Post;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import core.Post;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import service.PostService;
+
 @Component
+@Scope("prototype") // Tells springboot to create individual PostController class for posts
 public class PostController {
+  private final PostService postService;
+
+  @Autowired
+  @SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "We need to inject the "
+      + "userService and postService.")
+  public PostController(PostService postService) {
+    this.postService = postService;
+  }
 
   @FXML
   private Label displayNameLabel;
@@ -14,10 +29,40 @@ public class PostController {
   private Label usernameLabel;
   @FXML
   private Label contentLabel;
+  @FXML
+  private Button likeBtn;
+  @FXML
+  private Button retweetBtn;
 
+  private Post currentPost;
+
+  @FXML
   public void setData(Post post) {
+    this.currentPost = post;
+
     displayNameLabel.setText(post.getAuthor().getDisplayName());
     usernameLabel.setText("@" + post.getAuthor().getUsername());
     contentLabel.setText(post.getContent());
+    likeBtn.setText(post.getLikes() + " 👍");
+    retweetBtn.setText(post.getReTweets() + " 🔄");
+
+    // if (post.likedByUser(post.getAuthor()))
+    likeBtn.setStyle("-fx-text-fill: blue;");
+    // if (post.reTweetedByUser(userService.getLoggedInUser()))
+    // retweetBtn.setStyle("-fx-text-fill: cyan;");
+  }
+
+  @FXML
+  public void updateLikes() { // do not remove, posts wont show
+    if (currentPost != null) {
+      postService.likePost(currentPost);
+      likeBtn.setText(currentPost.getLikes() + " 👍");
+    }
+
+  }
+
+  @FXML
+  public void updateRetweets() { // do not remove, posts wont show
+
   }
 }
