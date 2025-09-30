d387b6551a7bb4b8cc17ab99656b4b8fbe70d429
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-09-30 13:39:19 +0200

    fix: close ui module

--- twitter-clone/ui/src/main/java/module-info.java
+++ twitter-clone/ui/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-open module twitter.ui {
+module twitter.ui {
   requires twitter.core;
   requires twitter.service;
 
@@ -13,4 +13,7 @@ open module twitter.ui {
   requires javafx.graphics;
 
   requires com.github.spotbugs.annotations;
+
+  opens ui to javafx.graphics, javafx.fxml, spring.core, spring.beans, spring.context;
+  opens ui.config to spring.core, spring.beans, spring.context;
 }
