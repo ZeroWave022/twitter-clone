0cb726a76d1418b914ae7582733cd6faba459654
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2025-11-04 14:04:24 +0100

    test: add user controller test

---
+++ twitter-clone/api/src/test/java/api/UserControllerTest.java
@@ -0,0 +1,50 @@
+package api;
+
+import core.payload.request.LoginRequest;
+import core.payload.response.LoginResponse;
+import core.payload.response.UserResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest()
+class UserControllerTest {
+
+  private RestTestClient restClient;
+  private String jwt;
+  private UserResponse user;
+
+  @BeforeEach
+  void setUp(WebApplicationContext context) {
+    restClient = RestTestClient.bindToApplicationContext(context)
+        .configureServer(mockMvc -> mockMvc.apply(SecurityMockMvcConfigurers.springSecurity())).build();
+
+    LoginRequest request = new LoginRequest("test user for api", "password");
+
+    LoginResponse response = restClient.post().uri("/auth/login").body(request).exchange()
+        .returnResult(LoginResponse.class).getResponseBody();
+
+    jwt = response.jwtToken();
+
+    user = restClient.get().uri("/auth/me").header("Authorization", "Bearer " + jwt).exchange()
+        .returnResult(UserResponse.class).getResponseBody();
+  }
+
+  @Test
+  void getUserWithId() {
+    UserResponse userResponse = restClient.get().uri("/api/users/" + user.id()).header("Authorization", "Bearer " + jwt)
+        .exchange()
+        .expectBody(UserResponse.class)
+        .returnResult()
+        .getResponseBody();
+
+    assertEquals(user.id(), userResponse.id());
+    assertEquals(user.username(), userResponse.username());
+  }
+}
